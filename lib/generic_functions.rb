b5d9ffdffb92967df31821c3dba7b9327cfcc522
Delon Newman <delnewman@salud.unm.edu>
2020-10-11 19:17:37 -0600

    Comment out test code

--- lib/generic_functions.rb
+++ lib/generic_functions.rb
@@ -67,20 +67,20 @@ module GenericFunctions
   end
 end
 
-include GenericFunctions
+# include GenericFunctions
 
-class Person; end
-class Jackie; end
-class Delon; end
+# class Person; end
+# class Jackie; end
+# class Delon; end
 
-multi :love, Delon, Jackie do |a, b|
-  puts "#{a} loves #{b}"
-end
+# multi :love, Delon, Jackie do |a, b|
+#   puts "#{a} loves #{b}"
+# end
 
-multi :love, Delon, Person do |a, b|
-  puts "#{a} loves Jackie, not sure about #{b}"
-end
+# multi :love, Delon, Person do |a, b|
+#   puts "#{a} loves Jackie, not sure about #{b}"
+# end
 
-multi :love, Jackie, Person do |a, b|
-  puts "#{a} loves Delon, not sure about #{b}"
-end
+# multi :love, Jackie, Person do |a, b|
+#   puts "#{a} loves Delon, not sure about #{b}"
+# end
